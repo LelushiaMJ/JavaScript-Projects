961069d4c2f487ad4ad3255a8d8b50e6df7b2b6f
Lelushia M Jamieson <lelujami@gmail.com>
2025-02-03 13:31:41 +0000

    JavaScript Projects 1
    First contribution to the JavaScript alert project.

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+// Assigning Variables
+
+var A = "This is a test string";
+var man = "Lelushia", woman = "Arna", country = "Wales", food = "Potato";
+var expressionAssignment = 5 + 21;
+
+// ---- Testing Alerts/Writing Options ----
+
+// alert("Hello World!");
+// window.alert("Hello World From Window!");
+// document.write("Hello World From The Document!");
+
+// document.write(A);
+// document.write("Concatenation " + "Assignment");
+// window.alert(A);
+// window.alert("Backslash \" Assignment \\");
+
